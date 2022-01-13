8057bbe3bf3398aea2d6d549e6c5f37913c72ad4
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2022-01-13 20:00:25 +0100

    Register routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
+  {path: 'register',  component: RegisterComponent, pathMatch: 'full'},
   {path: 'home', component: HomeComponent, pathMatch: 'full'},
-  {path: '', redirectTo: '/home', pathMatch: 'full'}
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({

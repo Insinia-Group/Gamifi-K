124bd085b49d2312d00ec5cf0bf45702dc39070a
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-14 15:53:57 +0100

    Elementos para testing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { fromEvent, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
 
+export class HomeComponent implements OnInit {
+  public isTestDivScrolledIntoView: boolean;
+  resizeObservable$: Observable<Event>
+  resizeSubscription$: Subscription 
+  
   constructor() { }
 
+  @HostListener('window:resize', ['$event'])
+  @HostListener('window:scroll', ['$event'])
+
   ngOnInit(): void {
+    this.resizeObservable$ = fromEvent(window, 'resize');
+    this.resizeSubscription$ = this.resizeObservable$.subscribe(evt => {
+    })
   }
 
+  onResize(event: any) {
+    console.log(event.target.innerWidth)
+    var element = document.getElementById('getStarted');
+    console.log(element?.getBoundingClientRect())
+  }
 }

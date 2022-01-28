67646eab46bc304f7860836e6af007097e01b213
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-01-28 19:53:35 +0100

    Creando estructura y funcionamiento de http service

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,11 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { API } from '../models/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-
+  public api: API;
   public response: string
 
-  constructor() { }
+  constructor(public http: HttpClient, public jwtHelper: JwtHelperService) {
+    this.api = new API();
+  }
+
+  status() {
+    this.http.post(this.api.toThisPath('/login'), ['asd']).subscribe(
+      (data) => console.log(data),
+      (err) => console.log(err)
+    );
+  }
 }

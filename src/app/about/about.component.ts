17fb5035e889bab7bf97edc090cca2db226abbfd
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2022-02-02 18:36:31 +0100

    about page purge sweetalert register

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  goHome() {
+    this.router.navigate(['/home']);
+}
 
 }

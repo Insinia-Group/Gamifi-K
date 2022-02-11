be41ab16b5e10a2c0dfb7a7cebf7a00fa4e61288
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-02-11 17:55:44 +0100

    Creando funcionamiento completo y correcto de los metodos de la clase API

--- src/app/models/api.ts
+++ src/app/models/api.ts
@@ -1,11 +1,36 @@
 export class API {
     private url: string;
+    private route: string;
 
     constructor() {
+        this.empty();
         this.url = 'http://localhost:3000';
     }
 
     toThisPath(path: string): string {
-        return this.url + path;
+        this.empty();
+        this.route = this.url + path;
+        return this.route;
+    }
+
+    addParameter(parameter: string, value: string): string {
+        this.route = this.route + this.checkChars(this.route) + parameter + '=' + value;
+        return this.route
+    }
+
+    addMultipleParameters(parameters: string[], values: string[]): string {
+        parameters.forEach((parameter, index) => {
+            this.route = this.route + this.checkChars(this.route) + parameter + '=' + values[index];
+        });
+        return this.route;
+    }
+
+    checkChars(path: string): string {
+        if (path.includes('?')) return '&';
+        else return '?';
+    }
+    
+    empty(): void {
+        this.route = '';
     }
 }

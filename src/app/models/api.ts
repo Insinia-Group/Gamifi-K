9a9661c95472176f5fc722cb5b0753a53e29e4dd
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-02-11 18:04:11 +0100

    Añadiendo comentarios de los metodos, sus parametros, los valores, el tipo y lo que devuelve en caso de no ser vacios

--- src/app/models/api.ts
+++ src/app/models/api.ts
@@ -2,22 +2,42 @@ export class API {
     private url: string;
     private route: string;
 
+    /**
+     * CONSTRUCTOR
+     */
     constructor() {
         this.empty();
         this.url = 'http://localhost:3000';
     }
 
+    /**
+     * Devuelve la url + el path que le hayamos pasado.
+     * @param path string
+     * @returns string
+     */
     toThisPath(path: string): string {
         this.empty();
         this.route = this.url + path;
         return this.route;
     }
 
+    /**
+     * Añade un parametro a la string y le añadie el valor.
+     * @param parameter string
+     * @param value string
+     * @returns string
+     */
     addParameter(parameter: string, value: string): string {
         this.route = this.route + this.checkChars(this.route) + parameter + '=' + value;
         return this.route
     }
 
+    /**
+     * Recorre el array parameters y añade el parametro más el valor del mismo indice del segundo array.
+     * @param parameters array string
+     * @param values array string
+     * @returns string
+     */
     addMultipleParameters(parameters: string[], values: string[]): string {
         parameters.forEach((parameter, index) => {
             this.route = this.route + this.checkChars(this.route) + parameter + '=' + values[index];
@@ -25,11 +45,19 @@ export class API {
         return this.route;
     }
 
+    /**
+     * Hace un check en caso de existir el valor ? y devuelve &, en caso contrario devolvera ?.
+     * @param path string
+     * @returns string
+     */
     checkChars(path: string): string {
         if (path.includes('?')) return '&';
         else return '?';
     }
     
+    /**
+     * Inicializa a vacio el atributo route.
+     */
     empty(): void {
         this.route = '';
     }

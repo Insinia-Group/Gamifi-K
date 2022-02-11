555ab20f3d95df0b49a2a8ad205bc021e217fb6b
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-02-11 17:55:27 +0100

    Creando test para utilizar la clase API

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { API } from '../models/api';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    const b = new API();
+    b.toThisPath('/asd');
+    console.log(b.addParameter('name', 'paco'));
+    console.log(b.addParameter('id', '3'));
+    console.log(b.addMultipleParameters(['edad', 'pais'], ['13', 'lituania']));
+    b.empty();
   }
-
 }

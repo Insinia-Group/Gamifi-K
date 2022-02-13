b932a7db7bd17519561939f553f2fa2d18db4bf8
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-02-13 18:12:45 +0100

    Añadiendo animaciones y funcionamiento completo del login con request al Backend

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { fadeIn } from '../config/animations.config';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [fadeIn]
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
+
+  constructor(private request: HttpService) {
+    this.request = request;
+  }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+    });
+  }
+
+  logIn() {
+    const user = {
+      email: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value
+    }
+    this.request.login(user);
+  }
 
 }

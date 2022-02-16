f2cf29c08273e8ad8c3ee6a9d34996787840e2bf
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-02-16 01:07:54 +0100

    Añadiendo nueva animacion

--- src/app/config/animations.config.ts
+++ src/app/config/animations.config.ts
@@ -46,6 +46,23 @@ trigger('slideInRight', [
   ]),
 ]
 
+export const slideInOut = trigger("slideInOut", [
+  transition(":enter", [
+    style({ opacity: 0, transform: "translateX(100%)" }), //apply default styles before animation starts
+    animate(
+      "750ms ease-in-out",
+      style({ opacity: 1, transform: "translateX(0)" })
+    )
+  ]),
+  transition(":leave", [
+    style({ opacity: 1, transform: "translateX(0)" }), //apply default styles before animation starts
+    animate(
+      "600ms ease-in-out",
+      style({ opacity: 0, transform: "translateX(100%)" })
+    )
+  ])
+])
+
 export const fadeIn = [
   /* FadeIn */
   trigger('fadeIn', [

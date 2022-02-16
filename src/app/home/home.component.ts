5511cb70147684e1fc99815e7205ba8bd2de6ac0
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-02-16 01:08:52 +0100

    Estructurando y haciendo funcionar el boton para volver a la parte superior

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,44 +1,36 @@
 import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
-import { slideIn, fadeIn } from '../config/animations.config';
-import { MessageHome } from '../models/MessageHome';
+import { slideIn, fadeIn, slideInOut } from '../config/animations.config';
 import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  animations: [slideIn, fadeIn]
+  animations: [slideIn, fadeIn, slideInOut]
 })
 
 export class HomeComponent implements AfterViewInit {
   public resizeSubscription$: Subscription;
   public resizeObservable$: Observable<Event>
   public isTestDivScrolledIntoView: boolean;
-  public message: MessageHome;
+  public goupStatus: boolean;
 
   constructor(public request: HttpService) {
-    this.message = new MessageHome();
     this.request = request;
     this.request.status();
+    this.goupStatus = false;
   }
 
-  @ViewChild('testDiv') testDiv: ElementRef;
-  @ViewChild('messageOne') messageOne: ElementRef
-  @ViewChild('messageTwo') messageTwo: ElementRef
-  @ViewChild('messageThree') messageThree: ElementRef
+  @ViewChild('go-up') goup: ElementRef;
   
   @HostListener('window:scroll', ['$event'])
   isScrolledIntoView() {
-    if (this.messageOne) {
-      this.message.messageOne = this.isElementOnScreen(this.messageOne)
-    };
-    if (this.messageTwo) {
-      this.message.messageTwo = this.isElementOnScreen(this.messageTwo)
-    };
-    if (this.messageThree) {
-      this.message.messageThree = this.isElementOnScreen(this.messageThree)
-    };
+    if (window.scrollY >= 500) {
+      this.goupStatus = true;
+    } else {
+      this.goupStatus = false;
+    }
   };
 
   isElementOnScreen(element: ElementRef): Boolean {
@@ -49,9 +41,6 @@ export class HomeComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    setTimeout(() => {
-      this.isScrolledIntoView();
-    });
   }
 
 }

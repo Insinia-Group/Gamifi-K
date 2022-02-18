9643f7abde220e8dd5b7525dbbd5b485afe2df86
Klau <klaudiusmiskinis@gmail.com>
2022-02-18 20:08:33 +0100

    Cambios de home y service jwt

--- src/app/services/jwt.service.ts
+++ src/app/services/jwt.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
 })
 export class JwtService {
 
-  constructor() { }
+  constructor(public JwtHelperService: JwtHelperService) {
+    
+  }
 }

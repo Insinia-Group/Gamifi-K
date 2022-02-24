660dee6cc72d72ae8f4188708b5a85ea887aa941
Klau <klaudiusmiskinis@gmail.com>
2022-02-24 17:47:54 +0100

    Añadiendo comentarios

--- src/app/services/jwt.service.ts
+++ src/app/services/jwt.service.ts
@@ -1,23 +1,37 @@
 import { Injectable } from '@angular/core';
-import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
 })
 export class JwtService {
 
+  /**
+   * Saves the token in the localStorage.
+   * @param token 
+   */
   setToken(token: any): void {
     localStorage.setItem('token', token)
   }
 
+  /**
+   * Return the token saved in localStorage.
+   * @returns String
+   */
   getToken(): any {
     return localStorage.getItem('token');
   }
 
+  /**
+   * Removes the token from the localStorage.
+   */
   remove(): void {
     localStorage.removeItem('token');
   }
 
+  /**
+   * Checks if the token exists.
+   * @returns Boolean
+   */
   exist(): boolean {
     if (this.getToken()) {
       return true;

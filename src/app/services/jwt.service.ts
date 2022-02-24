60d95509054d64c396b628c5b2cf10de14cf1239
Klau <klaudiusmiskinis@gmail.com>
2022-02-24 17:40:40 +0100

    Aplicando formato de codigo y eliminando constructor.

--- src/app/services/jwt.service.ts
+++ src/app/services/jwt.service.ts
@@ -1,39 +1,27 @@
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from "@auth0/angular-jwt";
 
-@Injectable( {
+@Injectable({
   providedIn: 'root'
-} )
-export class JwtService
-{
+})
+export class JwtService {
 
-
-  constructor ( public JwtHelperService: JwtHelperService )
-  {
-
-  }
-  setToken ( token: any ): void
-  {
-    localStorage.setItem( 'token', token )
+  setToken(token: any): void {
+    localStorage.setItem('token', token)
   }
 
-  getToken (): any
-  {
-    return localStorage.getItem( 'token' );
+  getToken(): any {
+    return localStorage.getItem('token');
   }
 
-  remove (): void
-  {
-    localStorage.removeItem( 'token' );
+  remove(): void {
+    localStorage.removeItem('token');
   }
 
-  exist (): boolean
-  {
-    if ( this.getToken() )
-    {
+  exist(): boolean {
+    if (this.getToken()) {
       return true;
-    } else
-    {
+    } else {
       return false;
     }
   }

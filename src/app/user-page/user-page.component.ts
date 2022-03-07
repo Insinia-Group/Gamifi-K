62f1e613f3c664f461c569b730e1bb9266db1f10
Klau <klaudiusmiskinis@gmail.com>
2022-03-07 17:52:20 +0100

    Creando el atributo profile y inicializandolo cuando le llega de la request al perfil

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { tempProfile } from '../models/profile';
 import { HttpService } from '../services/http.service';
 declare var $: any;
 
@@ -9,13 +10,15 @@ declare var $: any;
 })
 
 export class UserPageComponent implements OnInit {
-
+  public profile: tempProfile;
   constructor(private http: HttpService) {}
 
   @ViewChild('pictureProfile') pictureProfile: ElementRef;
 
-  ngOnInit(): void {
-    this.http.getProfile();
+  async ngOnInit(): Promise<void> {
+    const profile: any = await this.http.getProfile();
+    this.profile = new tempProfile(profile[0].id, profile[0].nick, profile[0].name, profile[0].lastName, profile[0].email, profile[0].description, profile[0].dateBirth, profile[0].avatar, profile[0].role, profile[0].dateJoined, profile[0].status);
+    console.log(this.profile);
   }
 
   toggleDropdown(id: string): void {

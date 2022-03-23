ee938c87d96f9df1658928916d59e2e394439a43
Klau <klaudiusmiskinis@gmail.com>
2022-03-23 20:01:32 +0100

    Cambiando Variables e incializando

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -5,6 +5,7 @@ import { isBase64 } from '../helpers/helpers';
 import { tempProfile } from '../models/profile';
 import { HttpService } from '../services/http.service';
 import { Router } from '@angular/router';
+import { ProfilePicture } from '../interface/image';
 declare var $: any;
 
 @Component({
@@ -16,8 +17,9 @@ declare var $: any;
 
 export class UserPageComponent implements OnInit {
   public profile: tempProfile;
-  public profileForm: FormGroup;
-  public profilePictureForm: FormGroup;
+  public profileForm: any;
+  public profilePictureForm: any;
+  public image: ProfilePicture;
 
   constructor(private http: HttpService, private router: Router) {
     this.profileForm = new FormGroup({
@@ -31,26 +33,27 @@ export class UserPageComponent implements OnInit {
       avatar: new FormControl('', [Validators.required, Validators.minLength(8)]),
     });
     this.setFormValues();
-    this.profilePictureForm = new FormGroup({
-      profilePicture: new FormControl(null, [Validators.required]),
-      profilePictureLabel: new FormControl()
-    })
+    this.image = {
+      base: undefined,
+      name: undefined,
+      size: undefined,
+      type: undefined,
+      ready: false
+    }
   }
 
-  @ViewChild('pictureProfile') pictureProfile: ElementRef;
+  @ViewChild('pictureProfile') profilePicture: ElementRef;
+  @ViewChild('profilePictureLabel') profilePictureLabel: ElementRef;
 
   async ngOnInit(): Promise<void> {
-
     if (localStorage.getItem('token') == null) {
       this.router.navigate(['/login']);
     }
-
     const statusToken = await this.http.tokenValidation();
     if (statusToken == false) {
       localStorage.removeItem('token');
       this.router.navigate(['/login']);
     }
-
     const profile: any = await this.http.getProfile();
     if (isBase64(profile[0].avatar)) {
       profile[0].avatar = atob(profile[0].avatar);
@@ -60,14 +63,31 @@ export class UserPageComponent implements OnInit {
   }
 
   setFormValues() {
+    const controls = this.profileForm.controls;
     if (this.profile) {
-      this.profileForm.controls.dateBirth.setValue(this.profile.dateBirth);
-      this.profileForm.controls.nick.setValue(this.profile.nick);
-      this.profileForm.controls.name.setValue(this.profile.name);
-      this.profileForm.controls.email.setValue(this.profile.email);
-      this.profileForm.controls.lastName.setValue(this.profile.lastName);
-      this.profileForm.controls.description.setValue(this.profile.description);
+      controls.dateBirth.setValue(this.profile.dateBirth);
+      controls.nick.setValue(this.profile.nick);
+      controls.name.setValue(this.profile.name);
+      controls.email.setValue(this.profile.email);
+      controls.lastName.setValue(this.profile.lastName);
+      controls.description.setValue(this.profile.description);
     }
+  }
+
+  async readURL(event: any) {
+    const file = event.target.files[0];
+    console.log(file)
+    if (file) {
+      this.image.name = file.name;
+      this.image.size = file.size;
+      this.image.type = file.type;
+      this.image.base = null;
+      this.image.ready = false;
+    }
+    const reader = new FileReader();
+    reader.onload = () => this.image.base = reader.result;
+    reader.readAsDataURL(file);
+    this.image.ready = true;
   }
 
   modal(id: string, state: string): void {
@@ -75,7 +95,7 @@ export class UserPageComponent implements OnInit {
   }
 
   updateProfilePicture() {
-    console.log('Update profile picture', this.profilePictureForm)
+    console.log('Update profile picture', this.image)
   }
 
   updateSubmit() {

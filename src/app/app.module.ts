3e12dae0181ed9c05822336d555fab0681cd340d
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-03-24 12:30:13 +0100

    Importando componente Logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
-import {APP_BASE_HREF} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
-import {FormsModule} from '@angular/forms';
-import {ReactiveFormsModule} from '@angular/forms';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AboutComponent} from './about/about.component';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-import {jwtConfig} from './config/jwt.config';
-import {NotFoundComponent} from './not-found/not-found.component';
-import {UserPageComponent} from './user-page/user-page.component';
-import {MatTableModule} from '@angular/material/table';
-import {MatInputModule} from '@angular/material/input';
-import {TokenInterceptor} from './interceptors/token-interceptor.interceptor';
-import {UserRankingComponent} from './user-ranking/user-ranking.component';
-import {AgGridModule} from 'ag-grid-angular';
-import {ModeratorPageComponent} from './moderator-page/moderator-page.component';
-import {LoadingComponent} from './loading/loading.component';
+import { APP_BASE_HREF } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './about/about.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { jwtConfig } from './config/jwt.config';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { TokenInterceptor } from './interceptors/token-interceptor.interceptor';
+import { UserRankingComponent } from './user-ranking/user-ranking.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { ModeratorPageComponent } from './moderator-page/moderator-page.component';
+import { LoadingComponent } from './loading/loading.component';
+import { LogoComponent } from './logo/logo.component';
 
 
 
@@ -36,7 +37,8 @@ import {LoadingComponent} from './loading/loading.component';
     UserPageComponent,
     UserRankingComponent,
     ModeratorPageComponent,
-    LoadingComponent
+    LoadingComponent,
+    LogoComponent
   ],
   imports: [
     MatTableModule,
@@ -51,9 +53,9 @@ import {LoadingComponent} from './loading/loading.component';
     AgGridModule.withComponents([])
   ],
   providers: [
-    {provide: APP_BASE_HREF, useValue: '/'},
-    {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true}
+    { provide: APP_BASE_HREF, useValue: '/' },
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }

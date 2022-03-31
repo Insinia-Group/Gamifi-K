b87fac45f4c1e68d17bc0defb9922d24b400425e
Klau <klaudiusmiskinis@gmail.com>
2022-03-31 19:44:15 +0200

    Creando objeto con configuracion angular-notifier

---
+++ src/app/config/notifier.config.ts
@@ -0,0 +1,41 @@
+import { NotifierOptions } from "angular-notifier";
+
+export const NotifierConfiguration: NotifierOptions = {
+    position: {
+        horizontal: {
+            position: 'right',
+            distance: 12
+        },
+        vertical: {
+            position: 'bottom',
+            distance: 32,
+            gap: 10
+        }
+    },
+    theme: 'material',
+    behaviour: {
+        autoHide: 3000,
+        onClick: 'hide',
+        onMouseover: 'pauseAutoHide',
+        stacking: 4
+    },
+    animations: {
+        enabled: true,
+        show: {
+            preset: 'slide',
+            speed: 300,
+            easing: 'ease'
+        },
+        hide: {
+            preset: 'slide',
+            speed: 300,
+            easing: 'ease',
+            offset: 50
+        },
+        shift: {
+            speed: 300,
+            easing: 'ease'
+        },
+        overlap: 150
+    }
+};

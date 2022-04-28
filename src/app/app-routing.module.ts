092438529c9b16707166d5a30cbb6656cccb7914
Klau <klaudiusmiskinis@gmail.com>
2022-04-28 20:05:21 +0200

    Añadiendo ruta del nuevo componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
-import {NgModule} from '@angular/core';
-import {ExtraOptions, RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {RegisterComponent} from './register/register.component';
-import {AboutComponent} from './about/about.component';
-import {LoginComponent} from './login/login.component';
-import {NotFoundComponent} from './not-found/not-found.component';
-import {UserPageComponent} from './user-page/user-page.component';
-import {UserRankingComponent} from './user-ranking/user-ranking.component';
-import {ModeratorPageComponent} from './moderator-page/moderator-page.component';
+import { NgModule } from '@angular/core';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { UserRankingComponent } from './user-ranking/user-ranking.component';
+import { ModeratorPageComponent } from './moderator-page/moderator-page.component';
+import { AddRankingComponent } from './add-ranking/add-ranking.component';
 
 const routerOptions: ExtraOptions = {
   useHash: false,
@@ -17,16 +18,17 @@ const routerOptions: ExtraOptions = {
 };
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent, pathMatch: 'full'},
-  {path: 'about', component: AboutComponent, pathMatch: 'full'},
-  {path: 'login', component: LoginComponent, pathMatch: 'full'},
-  {path: 'profile', component: UserPageComponent, pathMatch: 'full'},
-  {path: '404', component: NotFoundComponent, pathMatch: 'full'},
-  {path: 'register', component: RegisterComponent, pathMatch: 'full'},
-  {path: 'rankings', component: UserRankingComponent, pathMatch: 'full'},
-  {path: 'moderation', component: ModeratorPageComponent, pathMatch: 'full'},
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: '**', redirectTo: '/404'}
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+  { path: 'about', component: AboutComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'profile', component: UserPageComponent, pathMatch: 'full' },
+  { path: '404', component: NotFoundComponent, pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent, pathMatch: 'full' },
+  { path: 'rankings', component: UserRankingComponent, pathMatch: 'full' },
+  { path: 'moderation', component: ModeratorPageComponent, pathMatch: 'full' },
+  { path: 'create', component: AddRankingComponent, pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/404' }
 ];
 
 @NgModule({
@@ -34,4 +36,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 
-export class AppRoutingModule {}
+export class AppRoutingModule { }

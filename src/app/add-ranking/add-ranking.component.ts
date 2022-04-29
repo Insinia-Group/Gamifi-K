066a583a125c6518ab757d156ccef85118a6dc0e
Klau <klaudiusmiskinis@gmail.com>
2022-04-29 20:03:19 +0200

    Añadiendo campo para input de file

--- src/app/add-ranking/add-ranking.component.ts
+++ src/app/add-ranking/add-ranking.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { fadeIn } from '../config/animations.config';
+import { calculateSize } from '../helpers/helpers';
 
 @Component({
   selector: 'app-add-ranking',
@@ -8,10 +9,41 @@ import { fadeIn } from '../config/animations.config';
   animations: [fadeIn]
 })
 export class AddRankingComponent implements OnInit {
+  public image: any;
+  @ViewChild('rankingPicture') rankingPicture: ElementRef;
 
-  constructor() { }
+  constructor() {
+    this.image = {
+      base: undefined,
+      name: undefined,
+      size: undefined,
+      type: undefined,
+      ready: false,
+    };
+  }
 
   ngOnInit(): void {
+  }
+
+  async readURL(event: any) {
+    if (!event) throw 'No event provided';
+    const file = event.target.files[0];
+    if (!file) throw 'No file provided';
+    this.image.name = file.name;
+    this.image.size = file.size;
+    this.image.type = file.type;
+    this.image.base = null;
+    this.image.ready = false;
+    const reader = new FileReader();
+    reader.onload = () => (this.image.base = reader.result);
+    try {
+      reader.readAsDataURL(file);
+      this.image.ready = true;
+      this.rankingPicture.nativeElement.innerHTML =
+        this.image.name + ' ' + calculateSize(this.image.size);
+    } catch (e) {
+      throw 'Image selection cancelled';
+    }
   }
 
 }

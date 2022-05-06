059d9f75a41c94995331e676cc36a8085d7439f1
Klau <klaudiusmiskinis@gmail.com>
2022-05-06 19:09:05 +0200

    Creando Custom-validators

---
+++ src/app/helpers/custom-validators.ts
@@ -0,0 +1,45 @@
+import { AbstractControl } from "@angular/forms";
+
+export class CustomValidator {
+    static numeric(control: AbstractControl) {
+        let val = control.value;
+        if (val === null || val === '') return null;
+        if (!val.toString().match(/^[0-9]+(\.?[0-9]+)?$/)) return { 'invalidNumber': true };
+        return null;
+    }
+
+    static dateValidator(control: AbstractControl) {
+        let today: string | Date = new Date();
+        today = today.toISOString().slice(0, -14);
+        if (control.value < '1900-1-1') {
+            return { preOld: true };
+        } else if (control.value > today) {
+            return { postToday: true };
+        }
+        return null;
+    }
+
+    static hasExtension(control: AbstractControl) {
+        const value = control.value;
+        if (value.includes('.')) {
+            return { 'hasExtension': true }
+        }
+        return null;
+    }
+
+    static lastSlash(control: AbstractControl) {
+        const value = control.value;
+        if (value.slice(-1) !== '/') {
+            return { 'lastSlash': true }
+        }
+        return null;
+    }
+
+    static isUpperCase(control: AbstractControl) {
+        const value = control.value;
+        if (value.toUpperCase() !== value) {
+            return { 'notUpperCase': true }
+        }
+        return null;
+    }
+}

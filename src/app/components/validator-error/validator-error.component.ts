ddb9fcc4aad8f408b3c603302a9eb72d0beafa36
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-05-08 20:33:23 +0200

    Cambiando validadores

--- src/app/components/validator-error/validator-error.component.ts
+++ src/app/components/validator-error/validator-error.component.ts
@@ -6,7 +6,7 @@ import { fadeIn } from 'src/app/config/animations.config';
   selector: 'validator',
   templateUrl: './validator-error.component.html',
   styleUrls: ['./validator-error.component.css'],
-  animations: [fadeIn]
+  animations: [fadeIn],
 })
 export class ValidatorErrorComponent {
   @Input() ctrl: FormControl | any;
@@ -14,9 +14,11 @@ export class ValidatorErrorComponent {
 
   ERROR_MESSAGE: any = {
     required: () => `El campo es obligatorio.`,
+    email: () => `Debes introducir un email.`,
+    emailAlreadyExist: () => `Este email ya existe.`,
     minlength: (par: any) => `Mínimo ${par.requiredLength} caracteres.`,
     maxlength: (par: any) => `Máximo ${par.requiredLength} caracteres.`,
-    pattern: () => `No se permiten cáracteres especiales.`,
+    pattern: () => `Este formato no es válido.`,
     nameExists: () => `Este nombre ya existe.`,
     invalidNumber: () => `Debes introducir un número válido.`,
     preOld: () => `Fecha demasiado antigua.`,
@@ -26,10 +28,10 @@ export class ValidatorErrorComponent {
     max: (par: any) => `El valor máximo es ${par.max}.`,
     min: (par: any) => `El valor mínimo es ${par.min}.`,
     codeExist: () => `Este código no esta disponible.`,
-    notUpperCase: () => `Debe estar en mayúsculas.`
+    notUpperCase: () => `Debe estar en mayúsculas.`,
   };
 
-  constructor() { }
+  constructor() {}
 
   shouldShowErrors(): boolean {
     if (!this.isTouched) return this.ctrl && this.ctrl.errors;
@@ -37,8 +39,8 @@ export class ValidatorErrorComponent {
   }
 
   listOfErrors(): string[] {
-    return Object.keys(this.ctrl.errors).map(
-      err => this.ERROR_MESSAGE[err](this.ctrl.getError(err))
+    return Object.keys(this.ctrl.errors).map((err) =>
+      this.ERROR_MESSAGE[err](this.ctrl.getError(err))
     );
   }
 }

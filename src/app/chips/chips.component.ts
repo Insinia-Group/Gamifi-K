264b4e5f1f5f2719f0910d4603ceaef1ef7f57b0
Klau <klaudiusmiskinis@gmail.com>
2022-05-09 19:58:08 +0200

    Cambiando funcionamiento logico de chips

--- src/app/chips/chips.component.ts
+++ src/app/chips/chips.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   AbstractControl,
   FormControl,
@@ -6,6 +6,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { fadeIn } from '../config/animations.config';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-chips',
@@ -16,23 +17,24 @@ import { fadeIn } from '../config/animations.config';
 export class ChipsComponent {
   public emailArr: string[] = [];
   public addEmailForm: FormGroup;
+  public invalidEmails: string[] = [];
 
-  constructor() {
+  constructor(private http: HttpService) {
     this.addEmailForm = new FormGroup({
       email: new FormControl('', [
         Validators.required,
         Validators.pattern('^[a-z0-9._]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
         this.emailAlreadyExist.bind(this),
+        this.emailInvalid.bind(this)
       ]),
     });
   }
 
-  add(event: any) {
+  async add(event: any) {
     if (this.addEmailForm.valid) {
       const key = event.key;
       if (key == ',' || key == ' ' || key == 'Enter') {
-        this.emailArr.push(this.addEmailForm.controls.email.value);
-        this.addEmailForm.reset();
+        this.push();
       }
     }
   }
@@ -42,14 +44,29 @@ export class ChipsComponent {
     return { emailAlreadyExist: true };
   }
 
-  remove(removeEmail: string) {
-    this.emailArr = this.emailArr.filter((email) => removeEmail !== email);
-    console.log(this.emailArr);
+  emailInvalid(control: AbstractControl): { [key: string]: any } | null {
+    if (!this.emailArr.includes(control.value)) return null;
+    return { invalidEmail: true };
   }
 
-  addButton() {
+  remove(removeEmail: string) {
+    this.emailArr = this.emailArr.filter((email) => removeEmail !== email);
+  }
+
+  async push() {
+    const res: any = await this.http.emailExists(this.addEmailForm.controls.email.value);
+    console.log('invalidEmails:', this.invalidEmails)
+    console.log('res:', res)
+    if (!res.body.exists) {
+      this.addEmailForm.controls.email.setErrors({ emailNotExists: true })
+      return;
+    }
+    if (res.body.isAdmin) {
+      this.invalidEmails.push(this.addEmailForm.controls.email.value)
+      this.addEmailForm.controls.email.setErrors({ invalidEmail: true })
+      return;
+    }
     this.emailArr.push(this.addEmailForm.controls.email.value);
     this.addEmailForm.reset();
-    console.log(this.emailArr);
   }
 }

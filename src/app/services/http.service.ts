792f85db3bd000db3b39e5e4257eb8f1a2a2a4d9
Klau <klaudiusmiskinis@gmail.com>
2022-05-12 19:27:57 +0200

    Cambiando console.log(res)

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -57,6 +57,8 @@ export class HttpService {
           }
         },
         (err) => {
+          console.log(err);
+
           reject('Error with the login ');
         }
       );
@@ -92,7 +94,6 @@ export class HttpService {
       this.http.post<HttpResponse<any>>(this.api.toThisPath('/profile/image'), profile, { headers: headers }).subscribe(
         (res) => {
           if (res) {
-            console.log(res)
             resolve(res);
           } else {
             reject('Server Error');
@@ -146,7 +147,6 @@ export class HttpService {
     return new Promise((resolve, reject) => {
       this.http.post<any>(this.api.toThisPath('/removeUserFromRanking'), email, this.observe).subscribe(
         (res) => {
-          console.log(res);
           if (res.status == 200 && res.statusText == 'OK') {
             resolve(res.body);
           } else {
@@ -368,7 +368,6 @@ export class HttpService {
     return new Promise((resolve, reject) => {
       this.http.post<any>(this.api.toThisPath('/addUsersToRanking'), data, this.observe).subscribe(
         (res) => {
-          console.log(res);
           if (res.status == 200 && res.statusText == 'OK') {
             resolve(res.body);
           } else {
@@ -387,7 +386,6 @@ export class HttpService {
     return new Promise((resolve, reject) => {
       this.http.post<any>(this.api.toThisPath('/ranking'), data, this.observe).subscribe(
         (res) => {
-          console.log(res);
           if (res.status == 200 && res.statusText == 'OK') {
             resolve(res.body);
           } else {

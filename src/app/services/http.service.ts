42b5ed9f4b20c88e2df6aadc7bc7be2e1544e046
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-05-12 12:31:26 +0200

    Creando funcionamiento de getRankingUsersById

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { API } from '../models/api';
 import { Router } from '@angular/router';
 import { JwtService } from './jwt.service';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -104,14 +105,48 @@ export class HttpService {
     });
   }
 
-  getRankingUsersById(idRanking: number) {
-
+  getRankingUsersById(id: number) {
+    return new Promise((resolve, reject) => {
+      this.http.post<any>(this.api.toThisPath('/getRankingUsersById'), { idRanking: id }, this.observe).subscribe(
+        (res) => {
+          if (res.status == 200 && res.statusText == 'OK') {
+            resolve(res.body);
+          } else {
+            reject('Server Error');
+          }
+        },
+        (err) => {
+          console.log(err)
+          reject('Error getting the rankings.');
+        }
+      );
+    });
   }
 
   addRankingByCode(code: any): any {
     return new Promise((resolve, reject) => {
       this.http.post<any>(this.api.toThisPath('/addRankingByCode'), code, this.observe).subscribe(
         (res) => {
+          if (res.status == 200 && res.statusText == 'OK') {
+            resolve(res.body);
+          } else {
+            reject('Server Error');
+          }
+        },
+        (err) => {
+          console.log(err)
+          reject('Error getting the rankings.');
+        }
+      );
+    });
+  }
+
+
+  removeUserFromRanking(email: object) {
+    return new Promise((resolve, reject) => {
+      this.http.post<any>(this.api.toThisPath('/removeUserFromRanking'), email, this.observe).subscribe(
+        (res) => {
+          console.log(res);
           if (res.status == 200 && res.statusText == 'OK') {
             resolve(res.body);
           } else {

4d296f583d3d7b8cb944fa4e5d1ecc5c242de4da
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-05-12 23:36:26 +0200

    Añadiendo atributo

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -3,13 +3,13 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
-
 export class ModalComponent {
   @Input() uniqueId: string;
   @Input() title: any;
   @Input() isOpen: boolean = false;
   @Input() isAlert: boolean = false;
   @Input() isLogout: boolean = false;
+  @Input() isLarge: boolean = false;
 }

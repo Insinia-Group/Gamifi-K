2ac0711dbd0a14e2a0ef424970e408ea1bf4687f
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-05-13 10:26:44 +0200

    Cambiando UserPage por Profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { RegisterComponent } from './register/register.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { UserPageComponent } from './user-page/user-page.component';
 import { HistorialComponent } from './historial/historial.component';
 import { AddRankingComponent } from './add-ranking/add-ranking.component';
 import { UserRankingDevComponent } from './user-ranking-dev/user-ranking-dev.component';
 import { ChipsComponent } from './chips/chips.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routerOptions: ExtraOptions = {
   useHash: false,
@@ -22,7 +22,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent, pathMatch: 'full' },
   { path: 'about', component: AboutComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
-  { path: 'profile', component: UserPageComponent, pathMatch: 'full' },
+  { path: 'profile', component: ProfileComponent, pathMatch: 'full' },
   { path: '404', component: NotFoundComponent, pathMatch: 'full' },
   { path: 'register', component: RegisterComponent, pathMatch: 'full' },
   { path: 'history', component: HistorialComponent, pathMatch: 'full' },

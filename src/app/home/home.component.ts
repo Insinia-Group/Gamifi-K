3cc0d6f9b19925b5f65affdc7476ed0540576857
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-05-15 19:26:04 +0200

    Eliminando funcionamiento de mostrar  el topnav alternativo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,39 +1,15 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
-import { slideIn, fadeIn, slideInOut, slideDownHideUp } from '../config/animations.config';
+import { Component } from '@angular/core';
+import {
+  slideIn,
+  fadeIn,
+  slideInOut,
+  slideDownHideUp,
+} from '../config/animations.config';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  animations: [slideIn, fadeIn, slideInOut, slideDownHideUp]
+  animations: [slideIn, fadeIn, slideInOut, slideDownHideUp],
 })
-
-export class HomeComponent {
-  public resizeSubscription$: Subscription;
-  public resizeObservable$: Observable<Event>
-  public isTestDivScrolledIntoView: boolean;
-  public goupShow: boolean;
-  public topnavShow: boolean;
-  @ViewChild('go-up') goup: ElementRef;
-
-  constructor() {
-    this.goupShow = false;
-    this.topnavShow = false;
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  isScrolledIntoView() {
-    if (window.scrollY >= 250) {
-      this.goupShow = true;
-    } else {
-      this.goupShow = false;
-    }
-
-    if (window.scrollY >= 950) {
-      this.topnavShow = true;
-    } else {
-      this.topnavShow = false;
-    }
-  };
-}
+export class HomeComponent {}

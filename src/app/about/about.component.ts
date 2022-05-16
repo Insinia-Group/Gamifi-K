ef95907c7bf4fe160ef82586e34127a5d5918c9f
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-05-16 10:23:24 +0200

    Eliminando imports no utilizados y console.logs

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,27 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
 
   constructor(private router: Router) { }
 
-  
-  ngOnInit (): void
-  {
-    
-    
-  }
-
   goHome() {
-    this.router.navigate( [ '/home' ] );
-    console.log( "PENECORTO" );
-}
-
-
-  
-  
+    this.router.navigate(['/home']);
+  }
 }

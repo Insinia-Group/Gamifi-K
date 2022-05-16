53d7de1a5a9e3d519f514972e0557955d29f7f31
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-05-16 10:48:16 +0200

    Eliminando comentarios

--- src/app/models/rankings.ts
+++ src/app/models/rankings.ts
@@ -1,53 +1,15 @@
-
-export class Ranking 
-{
+export class Ranking {
     public id: number;
     public name: string;
     public description: string;
     public tipo: string;
     public logo: string;
-    
 
-    constructor ( id: number,description:string,logo:string,name:string,tipo:string )
-    {
+    constructor(id: number, description: string, logo: string, name: string, tipo: string) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.logo = logo;
         this.tipo = tipo;
     }
-
-    // GETTERS \\
-  /*   getId (){
-        return this.id;
-    }
-
-    getDescripcion (){
-        return this.description;
-    }
-
-    getLogo (){
-        return this.logo;
-    }
-    getName ()
-    {
-        return this.name;
-    }
-    // SETTERS \\
-
-    setId (id:number){
-         this.id = id;
-    }
-
-    setDescripcion (descripcion:string){
-        this.description = descripcion;
-    }
-
-    setLogo (logo:string){
-        this.logo = logo;
-    }
-    setName (name:string)
-    {
-       this.name =name;
-    } */
 }
